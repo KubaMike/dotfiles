17afb4921dfa9b22e80a9663285fccd7d0c64ac0
KubaMike <jmstefaniak2011@gmail.com>
2026-05-12 21:06:10 +0200

    Installed slides and node with brew in setup.yml.

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -6,4 +6,7 @@ sudo apt-get install ansible
 echo "Ansible installed."
 fi
 
+# Install ansible galaxy collections
+ansible-galaxy collection install community.general
+
 ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
